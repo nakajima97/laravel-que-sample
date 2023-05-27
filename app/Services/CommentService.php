181c048e173d040e727b0541ca0e5434fbbfbe63
Yudai Nakajima <kituneudon97@gmail.com>
2023-05-27 15:12:15 +0900

    #3 que使ったメリットわかりやすいように3秒待ってから保存するようにした

--- app/Services/CommentService.php
+++ app/Services/CommentService.php
@@ -8,6 +8,9 @@ class CommentService
 {
   public function store($author_name, $body)
   {
+    // 時間かかる処理にしたいので3秒待ってからDBに保存するようにする
+    sleep(3);
+
     Comment::create([
       "author_name" => $author_name,
       "body" => $body
